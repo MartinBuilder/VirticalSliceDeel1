12f2b75443ff724650ffc29bab97144b5c432ea9
Sjors van der Kleij <24689@ma-web.nl>
2018-11-28 11:34:44 +0100

    sjors commit
    commit

---
+++ Assets/Scripts/Score/scoreOnImpact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreOnImpact : MonoBehaviour {
+
+	
+	void Start () {
+        GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "wood");
+        Debug.Log("500 points");
+    }
+}
